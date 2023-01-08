08285a07a67e50f09d77a96a32c1a4f1cc7775ca
BrianMunene96 <107561877+BrianMunene96@users.noreply.github.com>
2023-01-08 15:51:03 +0100

    update preliminary work

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Fetch-API-example
-Learning how to fetch data from API with JSON Fake API
+
+Class assignment to fetch data from API with JSON Fake API
+
+Two buttons to fetch data from the backend
+
+- Get Users: fetches all user
+- Get User bu Id: fetches a specific user based on the id provided in the input field
+
+## Future Improvements
+
+- Separate works into different components that can be easily reused
+- Clear DOM elements before appending new elements
+- Simplify code 
---
+++ index.js
@@ -0,0 +1,95 @@
+// Placeholder keys
+const requiredKeys = [
+  'Name',
+  'Username',
+  'Email',
+  'Phone',
+  'Company',
+  'Website',
+  'City',
+  'ZipCode',
+];
+
+const url = 'https://jsonplaceholder.typicode.com/users';
+
+// Function to flatten o=nested object into single object
+const flattenObject = (obj) => {
+  const flattened = {};
+
+  Object.keys(obj).forEach((key) => {
+    const value = obj[key];
+
+    if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
+      Object.assign(flattened, flattenObject(value));
+    } else {
+      flattened[key] = value;
+    }
+  });
+
+  return flattened;
+};
+
+// Create a DOM element
+const createDomElement = (object) => {
+  const gridDiv = document.createElement('div');
+  gridDiv.className = 'grid-container';
+
+  for (const [key, value] of Object.entries(object)) {
+    requiredKeys.forEach((element) => {
+      if (lowercase(element) === key) {
+        const gridItem = document.createElement('div');
+        gridItem.className = 'grid-item';
+        gridItem.innerText = `${element}: ${value}`;
+        gridDiv.appendChild(gridItem);
+        const user = document.getElementsByClassName('users');
+
+        user[0].appendChild(gridDiv);
+        return gridItem;
+      }
+    });
+  }
+
+  return gridDiv;
+};
+
+// convert required keys to small caps
+function lowercase(string) {
+  return string.toLowerCase();
+}
+
+// First Button - Get users button
+const getUserButton = document.getElementById('get-users');
+
+// Fetch data from fake SON API nd handle it
+const getUsers = () => {
+  fetch(url)
+    .then((users) => users.json())
+    .then((data) => {
+      data.forEach((object) => {
+        const newObject = flattenObject(object);
+        const domElement = createDomElement(newObject);
+
+        return domElement;
+      });
+    });
+};
+
+getUserButton.addEventListener('click', getUsers);
+
+// Second Button - Get users by id button
+const getUserIdBtn = document.getElementById('get-users-id');
+
+const getUserById = () => {
+  const num = document.getElementById('user-input').value;
+  fetch(`${url}/${num}`)
+    .then((users) => users.json())
+    .then((object) => {
+      const newObject = flattenObject(object);
+      const domElement = createDomElement(newObject);
+      // domElement.innerHTML = "";
+
+      return domElement;
+    });
+};
+
+getUserIdBtn.addEventListener('click', getUserById);
